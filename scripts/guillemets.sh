0b7bb5ea89af0eed6f81cf249e145fdcb1eaf559
NicolasKAROLAK <nicolas@karolak.fr>
2014-10-09 22:03:04 +0200

    uniformisation guillemets - jeudi 9 octobre 2014, 22:03:04 (UTC+0200)

--- scripts/guillemets.sh
+++ scripts/guillemets.sh
@@ -3,8 +3,8 @@
 DIR='../tex/bjc_2014/*.tex'
 
 # guillemets
-sed -i 's/ "/ «~/g' $DIR
-sed -i 's/" /~» /g' $DIR
+sed -i 's/ "/ « /g' $DIR
+sed -i 's/" / » /g' $DIR
 sed -i 's/ ~"/ «~/g' $DIR
 sed -i 's/",/~»,/g' $DIR
 sed -i 's/"~/~»/g' $DIR
