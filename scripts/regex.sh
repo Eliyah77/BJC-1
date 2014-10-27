d5d12c2edb5c6deb8bf84de02426c1d6d978beb9
NicolasKAROLAK <nicolas@karolak.fr>
2014-10-27 04:31:06 +0100

    improve script

--- scripts/regex.sh
+++ scripts/regex.sh
@@ -4,23 +4,34 @@ TEX='../tex/bjc_2014/*.tex'
 
 #((\d\s*)*\w{1,3}\s*\.\s*\d{1,3}(\s*:\s*(\d{1,3})*(-*(\d{0,3})(\s*,\s*(\d{1,3})*-*(\d{1,3})*)*)*)*)
 
+# espaces
 sed -i 's/~/ /g' $TEX
 sed -i 's/ / /g' $TEX
+sed -i 's/	/ /g' $TEX
+sed -i 's/  / /g' $TEX
+sed -i 's/{ /{/g' $TEX
+sed -i 's/ }/}/g' $TEX
+
+# espaces & ponctuations
+sed -i 's/!/ !/g' $TEX
+sed -i 's/?/ ?/g' $TEX
+sed -i 's/;/ ;/g' $TEX
 sed -i 's/  / /g' $TEX
 sed -i 's/ ,/,/g' $TEX
+sed -i 's/ \./\./g' $TEX
+
+# ponctuations & caractères
 sed -i "s/’/'/g" $TEX
 sed -i 's/\.\.\./…/g' $TEX
-sed -i 's/ \./\./g' $TEX
+sed -i 's/oe/œ/g' $TEX
+sed -i 's/OE/Œ/g' $TEX
+
+# guillemets
 sed -i 's/ "/ « /g' $TEX
 sed -i 's/" / » /g' $TEX
 sed -i 's/",/ »,/g' $TEX
 sed -i 's/"\./ »\./g' $TEX
 sed -i 's/"}/ »}/g' $TEX
 sed -i 's/{"/{« /g' $TEX
-
-sed -i 's/!/ !/g' $TEX
-sed -i 's/?/ ?/g' $TEX
-sed -i 's/;/ ;/g' $TEX
-sed -i 's/  / /g' $TEX
 
 
