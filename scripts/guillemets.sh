27d48bf05f0fd1e6a72fc36c1f472ca6cb21eb31
NicolasKAROLAK <nicolas@karolak.fr>
2014-10-27 04:20:06 +0100

    improve script

--- scripts/guillemets.sh
+++ scripts/guillemets.sh
@@ -3,25 +3,6 @@
 DIR='../tex/bjc_2014/*.tex'
 
 # guillemets
-sed -i 's/ "/ «~/g' $DIR
-sed -i 's/" /~» /g' $DIR
-sed -i 's/ ~"/ «~/g' $DIR
-sed -i 's/",/~»,/g' $DIR
-sed -i 's/"~/~»/g' $DIR
-sed -i 's/"\./~»\./g' $DIR
-sed -i 's/\["/\[«~/g' $DIR
-sed -i 's/"\]/~»\]/g' $DIR
-sed -i 's/ ~/ «~/g' $DIR
-sed -i 's/~ /~» /g' $DIR
-sed -i 's/~\./~»\./g' $DIR
-sed -i 's/~,/~»,/g' $DIR
 sed -i 's/« /«~/g' $DIR
 sed -i 's/ »/~»/g' $DIR
-sed -i 's/{~/{«~/g' $DIR
-sed -i 's/~}/~»}/g' $DIR
-sed -i 's/(~/(«~/g' $DIR
-sed -i 's/~)/~»)/g' $DIR
 
-# crochets
-sed -i 's/\\TextTitle{\[/\\TextTitle{/g' $DIR
-sed -i 's/\]}/}/g' $DIR
