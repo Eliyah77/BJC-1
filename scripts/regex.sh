4cb58cf61f259a05119095dfb4fe350335facc23
NicolasKAROLAK <nicolas@karolak.fr>
2014-10-27 04:06:37 +0100

    add regex script

---
+++ scripts/regex.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TEX='./bjc_2014/*.tex'
+
+#((\d\s*)*\w{1,3}\s*\.\s*\d{1,3}(\s*:\s*(\d{1,3})*(-*(\d{0,3})(\s*,\s*(\d{1,3})*-*(\d{1,3})*)*)*)*)
+
+sed -i 's/~/ /g' $TEX
+sed -i 's/ / /g' $TEX
+sed -i 's/  / /g' $TEX
+sed -i 's/ ,/,/g' $TEX
+sed -i "s/’/'/g" $TEX
+sed -i 's/\.\.\./…/g' $TEX
+sed -i 's/ \./\./g' $TEX
+sed -i 's/ "/ « /g' $TEX
+sed -i 's/" / » /g' $TEX
+sed -i 's/",/ »,/g' $TEX
+sed -i 's/"\./ »\./g' $TEX
+
