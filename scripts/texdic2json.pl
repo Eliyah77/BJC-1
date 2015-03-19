12c6fb7c64ff04b32629c1953e1a5d9caa18e732
NicolasKAROLAK <nicolas@karolak.fr>
2015-03-19 11:06:50 +0100

    new src_file path

--- scripts/texdic2json.pl
+++ scripts/texdic2json.pl
@@ -8,7 +8,7 @@ use File::Basename;
 # variables
 my $name = 'dictionnaire';
 my $dirname = dirname(__FILE__);
-my $src_file = $dirname.'/../tex/bjc/annexes/'.$name.'.tex';
+my $src_file = $dirname.'/../tex/bjc/aides/'.$name.'.tex';
 my $dst_file = $dirname.'/../txt/'.$name.'.json';
 my ($src_fh, $dst_fh);
 my ($line, $word, $etymology, $definition);
